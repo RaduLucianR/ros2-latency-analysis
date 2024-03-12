195a84e2f70c6f6314cc0f27b21bbb27efb68e1f
LucianRadul <radlucradulescu@gmail.com>
2024-03-12 10:59:04 +0100

    add option to process specific folder

--- scripts/retrieve_traces.bash
+++ scripts/retrieve_traces.bash
@@ -5,7 +5,7 @@ default_source_folder="/home/pi/.ros/tracing"
 default_destination_folder="/home/${USER}/tracing"
 default_user="pi"
 default_password="raspberry"
-all_folders=false
+default_folders="latest"
 
 # Function to display usage
 usage() {
@@ -16,7 +16,7 @@ usage() {
     echo "  -d destination folder (optional): path as string, default is $default_destination_folder"
     echo "  -u user for the Raspberry Pi (optional), default is $default_user"
     echo "  -p password for the Raspberry Pi (optional), default is $default_password"
-    echo "  -a process all trace folders (optional)"
+    echo "  -f which sub-folders of the source folder to process (optional): latest (default) | specific | all"
     exit 1
 }
 
@@ -26,7 +26,7 @@ if [ $# -eq 0 ]; then
 fi
 
 # Parse the command-line arguments
-while getopts ":i:s:d:u:p:a" opt; do
+while getopts ":i:s:d:u:p:f:" opt; do
   case $opt in
     i)
       IP=$OPTARG
@@ -43,8 +43,8 @@ while getopts ":i:s:d:u:p:a" opt; do
     p)
       password=$OPTARG
       ;;
-    a)
-      all_folders=true
+    f)
+      folder_option=$OPTARG
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -68,35 +68,75 @@ source_folder=${source_folder:-$default_source_folder}
 destination_folder=${destination_folder:-$default_destination_folder}
 user=${user:-$default_user}
 password=${password:-$default_password}
+folder_option=${folder_option:-$default_folders}
+
+# Remove the options processed by getopts
+shift $((OPTIND-1))
+
+if [ "$folder_option" = "specific" ]; then
+    if [ $# -eq 0 ]; then
+        echo "No folder name specified for the 'specific' option."
+        exit 1
+    fi
+    folder_name=$1
+    shift
+else
+    folder_name=""  # Clear folder_name as we don't need it for 'latest' or 'all'
+fi
 
 # Source ROS2 and CARET
 source /opt/ros/humble/setup.bash
 source ~/ros2_caret_ws/install/local_setup.bash
 
-if [ "$all_folders" = true ]; then
-  archive_name="traces_$(date +%Y%m%d_%H%M%S).tar.gz"
-  sshpass -p "${password}" ssh "${user}"@"${IP}" "tar -czvf ${source_folder}/${archive_name} -C ${source_folder} ."
-  sshpass -p "${password}" scp "${user}"@"${IP}":"${source_folder}/${archive_name}" ${destination_folder}
-  sshpass -p "${password}" ssh "${user}"@"${IP}" "rm -rf ${source_folder}/${archive_name}"
-  tar -xzvf ${destination_folder}/${archive_name} -C ${destination_folder}
-  rm -rf ${destination_folder}/${archive_name}
+case $folder_option in
+  latest)
+    latest_created_folder=$(sshpass -p "${password}" ssh "${user}"@"${IP}" "ls -lt ${source_folder} | grep ^d | head -n 1 | awk '{print \$9}'")
+    folder_name=$latest_created_folder
+    sshpass -p "${password}" ssh "${user}"@"${IP}" "tar -czvf ${source_folder}/${folder_name}.tar.gz -C ${source_folder} ${folder_name}"
+    sshpass -p "${password}" scp "${user}"@"${IP}":"${source_folder}/${folder_name}.tar.gz" ${destination_folder}
+    sshpass -p "${password}" ssh "${user}"@"${IP}" "rm -rf ${source_folder}/${folder_name}.tar.gz"
+    tar -xzvf ${destination_folder}/${folder_name}.tar.gz -C ${destination_folder}
+    rm -rf ${destination_folder}/${folder_name}.tar.gz
+    ros2 caret create_architecture_file ${destination_folder}/${folder_name} -o ${destination_folder}/archi-${folder_name}.yaml
+    exit 0
+    ;;
 
-  for subdir in "$destination_folder"/*; do
-    # Check if the item is a directory
-    if [ -d "$subdir" ]; then
-        echo "Processing trace folder: $subdir"
-        subdir_f="${subdir##*/}"
-        ros2 caret create_architecture_file ${subdir} -o ${destination_folder}/archi-${subdir_f}.yaml
+  specific)
+    if [ -z "$folder_name" ]; then
+      echo "Folder name must be specified with the 'specific' option."
+      exit 1
     fi
-  done
-else
-  # Retrieve the latest created sub-folder
-  latest_created_folder=$(sshpass -p "${password}" ssh "${user}"@"${IP}" "ls -lt ${source_folder} | grep ^d | head -n 1 | awk '{print \$9}'")
-  folder_name=$latest_created_folder
-  sshpass -p "${password}" ssh "${user}"@"${IP}" "tar -czvf ${source_folder}/${folder_name}.tar.gz -C ${source_folder} ${folder_name}"
-  sshpass -p "${password}" scp "${user}"@"${IP}":"${source_folder}/${folder_name}.tar.gz" ${destination_folder}
-  sshpass -p "${password}" ssh "${user}"@"${IP}" "rm -rf ${source_folder}/${folder_name}.tar.gz"
-  tar -xzvf ${destination_folder}/${folder_name}.tar.gz -C ${destination_folder}
-  rm -rf ${destination_folder}/${folder_name}.tar.gz
-  ros2 caret create_architecture_file ${destination_folder}/${folder_name} -o ${destination_folder}/archi-${folder_name}.yaml
-fi
+
+    sshpass -p "${password}" ssh "${user}"@"${IP}" "tar -czvf ${source_folder}/${folder_name}.tar.gz -C ${source_folder} ${folder_name}"
+    sshpass -p "${password}" scp "${user}"@"${IP}":"${source_folder}/${folder_name}.tar.gz" ${destination_folder}
+    sshpass -p "${password}" ssh "${user}"@"${IP}" "rm -rf ${source_folder}/${folder_name}.tar.gz"
+    tar -xzvf ${destination_folder}/${folder_name}.tar.gz -C ${destination_folder}
+    rm -rf ${destination_folder}/${folder_name}.tar.gz
+    ros2 caret create_architecture_file ${destination_folder}/${folder_name} -o ${destination_folder}/archi-${folder_name}.yaml
+    exit 0
+    ;;
+
+  all)
+    archive_name="traces_$(date +%Y%m%d_%H%M%S).tar.gz"
+    sshpass -p "${password}" ssh "${user}"@"${IP}" "tar -czvf ${source_folder}/${archive_name} -C ${source_folder} ."
+    sshpass -p "${password}" scp "${user}"@"${IP}":"${source_folder}/${archive_name}" ${destination_folder}
+    sshpass -p "${password}" ssh "${user}"@"${IP}" "rm -rf ${source_folder}/${archive_name}"
+    tar -xzvf ${destination_folder}/${archive_name} -C ${destination_folder}
+    rm -rf ${destination_folder}/${archive_name}
+
+    for subdir in "$destination_folder"/*; do
+      # Check if the item is a directory
+      if [ -d "$subdir" ]; then
+          echo "Processing trace folder: $subdir"
+          subdir_f="${subdir##*/}"
+          ros2 caret create_architecture_file ${subdir} -o ${destination_folder}/archi-${subdir_f}.yaml
+      fi
+    done
+
+    exit 0
+    ;;
+  *)
+    echo "Invalid folder option: $folder_option"
+    exit 1
+    ;;
+esac
