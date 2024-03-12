2e936e92832e4ba1bb07536a481628654df84dcc
LucianRadul <radlucradulescu@gmail.com>
2024-03-12 14:25:39 +0100

    rewrite getMsgOfSize as inline func

--- simple-chain/include/msg_size.hpp
+++ simple-chain/include/msg_size.hpp
@@ -1,34 +1,32 @@
-#ifndef MESSAGE_PRESETS_HPP
-#define MESSAGE_PRESETS_HPP
+#ifndef MSG_SIZE_HPP
+#define MSG_SIZE_HPP
 
 #include <string>
 #include <regex>
 #include "std_msgs/msg/string.hpp"
 
-class MessagePresets {
-public:
-    static std_msgs::msg::String getMsgOfSize(const std::string& size_str) {
-        size_t size_in_bytes = parsePayloadSize(size_str);
-        std_msgs::msg::String msg;
-        msg.data = std::string(size_in_bytes, 'x'); // Fill with 'x' characters or any character you prefer
-        return msg;
+inline std_msgs::msg::String getMsgOfSize(const std::string& size_str) {
+    size_t size_in_bytes;
+
+    static const std::regex size_regex(R"((\d+)([Kk]?B)?)");
+    std::smatch match;
+    if (std::regex_match(size_str, match, size_regex)) {
+        size_t multiplier = 1;
+
+        if (match[2].str() == "KB" || match[2].str() == "kb") {
+            multiplier = 1024;
+        }
+
+        size_in_bytes = std::stoi(match[1].str()) * multiplier;
+    } else {
+        // Default to 128 bytes if invalid format
+        size_in_bytes = 128;
     }
 
-private:
-    static size_t parsePayloadSize(const std::string& size_str) {
-        static const std::regex size_regex(R"((\d+)([Kk]?B)?)");
-        std::smatch match;
-        if (std::regex_match(size_str, match, size_regex)) {
-            size_t multiplier = 1;
-            if (match[2].str() == "KB" || match[2].str() == "kb") {
-                multiplier = 1024;
-            }
-            return std::stoi(match[1].str()) * multiplier;
-        } else {
-            // Default to 128 bytes if invalid format
-            return 128;
-        }
-    }
-};
+    std_msgs::msg::String msg;
+    msg.data = std::string(size_in_bytes, 'x'); // Fill with 'x' characters or any character you prefer
+    
+    return msg;
+}
 
 #endif
