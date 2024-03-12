d8fb1225e38399ffd285875c907ea58e3b356a73
LucianRadul <radlucradulescu@gmail.com>
2024-03-12 12:19:30 +0100

    actuator cpp rewrite

--- simple-chain/src/tasks/actuator.cpp
+++ simple-chain/src/tasks/actuator.cpp
@@ -3,21 +3,14 @@
 
 using std::placeholders::_1;
 
-class Actuator : public rclcpp::Node
+Actuator::Actuator()
+: Node("actuator")
 {
-  public:
-    Actuator()
-    : Node("actuator")
-    {
-      subscription_ = this->create_subscription<std_msgs::msg::String>(
-      "force", 10, std::bind(&Actuator::topic_callback, this, _1));
-    }
+  subscription_ = this->create_subscription<std_msgs::msg::String>(
+    "force", 10, std::bind(&Actuator::topic_callback, this, _1));
+}
 
-  private:
-    void topic_callback(const std_msgs::msg::String & msg) const
-    {
-      RCLCPP_INFO(this->get_logger(), "I apply force: '%s'", msg.data.c_str());
-    }
-    
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
-};
+void Actuator::topic_callback(const std_msgs::msg::String & msg) const
+{
+  RCLCPP_INFO(this->get_logger(), "I apply force: '%s'", msg.data.c_str());
+}
