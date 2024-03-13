59e149ced6e41b3e1f40cacdbcaf03cb10915883
LucianRadul <radlucradulescu@gmail.com>
2024-03-13 13:08:26 +0100

    add launch files forall exec archi

---
+++ simple-chain/launch/1-ste-launch.py
@@ -0,0 +1,44 @@
+from launch import LaunchDescription
+from launch.actions import TimerAction, Shutdown, DeclareLaunchArgument
+from launch_ros.actions import Node
+from tracetools_launch.action import Trace
+from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
+import os
+import time
+
+def generate_launch_description():
+    executors = '1-ste'
+    
+    config_payload = os.path.join(
+      get_package_share_directory('simple-chain'),
+      'config',
+      'msg_sizes.yaml'
+      )
+    
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'trfolder',  # Name of the argument
+            default_value=time.strftime(f"e2e_{executors}_%Y%m%d-%H%M%S"),  # Default value
+            description='Trace folder name'
+        ),
+
+        Trace(
+            session_name=LaunchConfiguration('trfolder'),
+            events_kernel=[],
+            events_ust=['ros2*']
+        ),
+
+        Node(
+            package='simple-chain',
+            executable=executors,
+            parameters=[config_payload]
+        ),
+
+        TimerAction(
+            period=10.0,  # time in seconds after which the shutdown will be triggered
+            actions=[
+                Shutdown(reason='Time limit reached')
+            ]
+        )
+    ])
